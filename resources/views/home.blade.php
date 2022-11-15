6b3b58ab83158ee939e88226f27a515ce7b93978
lennc <lenn.crochart@student.howest.be>
2022-11-15 10:58:53 +0100

    some HTML and CSS tinkering

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,7 +9,31 @@
 </head>
 <body>
 
-    <p>test</p>
+    <h1>Welcome to the potion hub!</h1>
+    <img src="{{asset('img/wizard-mage.gif')}}" alt="wizard-gif" class="wizard"> {{-- does not work, fix this! --}}
+
+    <div class="container">
+        <div class="col">
+            <h2>Ingredients!</h2>
+            <div>
+                <ul>
+                    {{-- @foreach ($ingredients as $ingredient)
+                        <li>{{$ingredient->name}}</li>
+                    @endforeach--}}
+                </ul>
+            </div>
+        </div>
+
+        <div class="col">
+            <h2>Make your potion!</h2>
+
+        </div>
+
+        <div class="col">
+            <h2>Brewed potions</h2>
+
+        </div>
+    </div>
 
 </body>
 </html>

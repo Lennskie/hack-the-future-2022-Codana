8b6dab6bb3193f0d10c063f99df520b18dab5002
Nicolas Loontjens <nicolas.loontjens@student.howest.be>
2022-11-15 14:19:21 +0100

    added deletion of selected ingredients in gui

--- public/js/index.js
+++ public/js/index.js
@@ -18,10 +18,34 @@ function addIngredientToCauldron(e){
     ingredients.push(ingredient);
     console.log(ingredients);
     checkIngredients();
+    updateGui();
 }
 
 function checkIngredients(){
     if(ingredients.length > 0){
         document.querySelector(".button").style = "display: block";
     }
+}
+
+function updateGui(){
+    document.querySelector(".currentPotionList").innerHTML = "";
+    ingredients.forEach(ingredient => {
+        document.querySelector(".currentPotionList").innerHTML += `<img item-id="${ingredient.id}"  src="/img/${ingredient.name}.png">`
+    });
+    document.querySelectorAll('.currentPotionList img').forEach(elem => {
+        elem.addEventListener("click", removeIngredient);
+    });
+}
+
+function removeIngredient(e){
+    console.log(e.currentTarget);
+    let exit = false;
+    ingredients.forEach(elem => {
+        if(exit) return;
+        if(elem.id == e.currentTarget.getAttribute('item-id')){
+            ingredients.splice(ingredients.indexOf(elem), 1);
+            exit = true;
+        }
+    });
+    updateGui();
 }

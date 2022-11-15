5f4c895febb814573bea05d1c928e99055e63542
lennc <lenn.crochart@student.howest.be>
2022-11-15 11:02:16 +0100

    Better HMTL syntax

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,32 +9,35 @@
     <title>Department of Variety Gaming - Hack The Future 2022</title>
 </head>
 <body>
+    <header>
+        <h1>Welcome to the potion hub!</h1>
+        <img src="{{asset('img/wizard-mage.gif')}}" alt="wizard-gif" class="wizard">
+    </header>
+    <main>
+        <div class="container">
+            <div class="col">
+                <h2>Ingredients!</h2>
+                <div>
+                    <ul>
+                        {{-- @foreach ($ingredients as $ingredient)
+                            <li>{{$ingredient->name}}</li>
+                        @endforeach--}}
+                    </ul>
+                </div>
+            </div>
 
-    <h1>Welcome to the potion hub!</h1>
-    <img src="{{asset('img/wizard-mage.gif')}}" alt="wizard-gif" class="wizard"> {{-- does not work, fix this! --}}
+            <div class="col">
+                <h2>Make your potion!</h2>
 
-    <div class="container">
-        <div class="col">
-            <h2>Ingredients!</h2>
-            <div>
-                <ul>
-                    {{-- @foreach ($ingredients as $ingredient)
-                        <li>{{$ingredient->name}}</li>
-                    @endforeach--}}
-                </ul>
+            </div>
+
+            <div class="col">
+                <h2>Brewed potions</h2>
+
             </div>
         </div>
-
-        <div class="col">
-            <h2>Make your potion!</h2>
-
-        </div>
-
-        <div class="col">
-            <h2>Brewed potions</h2>
-
-        </div>
-    </div>
-
+    </main>
+    <footer>
+    </footer>
 </body>
 </html>
